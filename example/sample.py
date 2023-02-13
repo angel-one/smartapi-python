467134f1db28b7bda272c9909dab43881ba989dc
ahbijeet bote <abhijeet.bote@acc.ltd>
2023-02-13 19:38:28 +0530

    websoclet2 api

--- example/sample.py
+++ example/sample.py
@@ -1,13 +1,25 @@
 # package import statement
-from smartapi import SmartConnect #or from smartapi.smartConnect import SmartConnect
+from SmartApi import SmartConnect #or from smartapi.smartConnect import SmartConnect
 #import smartapi.smartExceptions(for smartExceptions)
 
+
+apiKey = "<YOUR API KEY>"
+clientId = "<YOUR CLIENT ID>"
+pin = "<PIN>"
+totp = "<TOTP>"
+
 #create object of call
-obj=SmartConnect(api_key="your api key")
+obj=SmartConnect(api_key=apiKey)
+print('obj : ',obj)
 
 #login api call
 
-data = obj.generateSession("Your Client ID","Your Password","Your totp here")
+data = obj.generateSession(clientId,pin,totp)
+print('data',data)
+
+authToken = data['data']['jwtToken']
+authToken = f'Bearer {authToken}'
+
 refreshToken= data['data']['refreshToken']
 
 #fetch the feedtoken
@@ -31,8 +43,8 @@ try:
         "stoploss": "0",
         "quantity": "1"
         }
-    orderId=obj.placeOrder(orderparams)
-    print("The order id is: {}".format(orderId))
+    # orderId=obj.placeOrder(orderparams)
+    # print("The order id is: {}".format(orderId))
 except Exception as e:
     print("Order placement failed: {}".format(e.message))
 #gtt rule creation
@@ -75,7 +87,7 @@ try:
     obj.getCandleData(historicParam)
 except Exception as e:
     print("Historic Api failed: {}".format(e.message))
-#logout
+logout
 try:
     logout=obj.terminateSession('Your Client Id')
     print("Logout Successfull")
@@ -84,30 +96,83 @@ except Exception as e:
 
 
 
-## WebSocket
-from smartapi import WebSocket
+# ## WebSocket
+# from SmartApi import WebSocket
 
-FEED_TOKEN= "your feed token"
-CLIENT_CODE="your client Id"
-token="channel you want the information of" #"nse_cm|2885&nse_cm|1594&nse_cm|11536"
-task="task" #"mw"|"sfi"|"dp"
-ss = WebSocket(FEED_TOKEN, CLIENT_CODE)
+# FEED_TOKEN= "your feed token"
+# CLIENT_CODE="your client Id"
+# token="channel you want the information of" #"nse_cm|2885&nse_cm|1594&nse_cm|11536"
+# task="task" #"mw"|"sfi"|"dp"
+# ss = WebSocket(FEED_TOKEN, CLIENT_CODE)
 
-def on_tick(ws, tick):
-    print("Ticks: {}".format(tick))
+# def on_tick(ws, tick):
+#     print("Ticks: {}".format(tick))
 
-def on_connect(ws, response):
-    ws.websocket_connection() # Websocket connection  
-    ws.send_request(token,task) 
+# def on_connect(ws, response):
+#     ws.websocket_connection() # Websocket connection  
+#     ws.send_request(token,task) 
     
-def on_close(ws, code, reason):
-    ws.stop()
+# def on_close(ws, code, reason):
+#     ws.stop()
+
+# # Assign the callbacks.
+# ss.on_ticks = on_tick
+# ss.on_connect = on_connect
+# ss.on_close = on_close
+
+# ss.connect()
+
+
+
+from SmartApi.smartWebSocketV2 import SmartWebSocketV2
+
+
+
+AUTH_TOKEN = authToken
+API_KEY = 'uU2XbJU1'
+CLIENT_CODE = 'A50193197'
+FEED_TOKEN = feedToken
+
+correlation_id = "abhijeet_123_qwerty"
+action = 1
+mode = 1
+
+token_list = [
+               {
+                    "exchangeType": 5,
+                    "tokens": [
+                         "244999",
+                         "246083"
+                    ]
+               }]
+
+# token_list = [{"exchangeType": 1, "tokens": ["26009"]}]
+
+sws = SmartWebSocketV2(AUTH_TOKEN, API_KEY, CLIENT_CODE, FEED_TOKEN)
+
+def on_data(wsapp, message):
+    print("Ticks: {}".format(message))
+
+
+def on_open(wsapp):
+    print("on open")
+    sws.subscribe(correlation_id, mode, token_list)
+
+
+def on_error(wsapp, error):
+    print(error)
+
+
+def on_close(wsapp):
+    print("Close")
+
 
 # Assign the callbacks.
-ss.on_ticks = on_tick
-ss.on_connect = on_connect
-ss.on_close = on_close
+sws.on_open = on_open
+sws.on_data = on_data
+sws.on_error = on_error
+sws.on_close = on_close
 
-ss.connect()
+sws.connect()
 
 
